981f68ecdccc482e1158737be8548f0da891fb9c
Vu Anh <brother.rain.1024@gmail.com>
2017-08-03 15:24:39 +0700

    fix bug

--- distill/__init__.py
+++ distill/__init__.py
@@ -1,19 +1,7 @@
 # -*- coding: utf-8 -*-
-
 __author__ = """Vu Anh"""
 __email__ = 'brother.rain.1024@gmail.com'
-__version__ = '1.1.1'
-
-import sys
-
-if sys.version_info >= (3, 0):
-    from underthesea.word_sent.word_sent import word_sent
-    from underthesea.pos_tag.pos_tag import pos_tag
-    from underthesea.chunking.chunk import chunk
-else:
-    from word_sent.word_sent import word_sent
-    from pos_tag.pos_tag import pos_tag
-    from chunking.chunk import chunk
+__version__ = '0.0.2'
 
 
 def info(version):
@@ -23,4 +11,4 @@ def info(version):
     :type version: str
     """
     print(version)
-    print("underthesea is a Vietnamese NLP Toolkit")
+    print("distill is doc platform for coder")
--- distill/cli.py
+++ distill/cli.py
@@ -6,7 +6,8 @@ import click
 @click.command()
 def main(args=None):
     """Console script for distill"""
-    click.echo("Hello World")
+    click.echo("Welcome to distill.py!")
+    click.echo("Happy coding and writing!")
 
 
 if __name__ == "__main__":
